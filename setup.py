6ef6ae28d98ae7e1473fc22657bd6008de7752db
Jonas Baumann <jone@jone.ch>
2012-03-26 17:17:21 +0200

    Create package with paster.

---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+maintainer = 'Jonas Baumann'
+
+tests_require = [
+    'plone.testing',
+    'ftw.testing',
+    ]
+
+extras_require = {
+    'tests': tests_require}
+
+setup(name='ftw.bridge.client',
+      version=version,
+      description='Adds ftw.bridge support to plone.',
+
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        ],
+
+      keywords='ftw bridge proxy client',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.bridge.client',
+      license='GPL2',
+
+      packages=find_packages(),
+      namespace_packages=['ftw', 'ftw.bridge'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+          'setuptools',
+      ],
+      tests_require=tests_require,
+      extras_require=extras_require,
+
+      entry_points='''
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
+      )
