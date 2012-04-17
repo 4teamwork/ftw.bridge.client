26fe1e15dcfdd5cf01e4d68d9bfcd688750869a1
Jonas Baumann <jone@jone.ch>
2012-04-17 11:32:21 +0200

    Implement remote-add-favorite view.

--- ftw/bridge/client/browser/favorite.py
+++ ftw/bridge/client/browser/favorite.py
@@ -1,5 +1,12 @@
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
+from Products.statusmessages.interfaces import IStatusMessage
+from ftw.bridge.client import _
+from ftw.bridge.client.exceptions import MaintenanceError
+from ftw.bridge.client.interfaces import IBridgeRequest
+from ftw.bridge.client.interfaces import MAINTENANCE_ERROR_MESSAGE
+from ftw.bridge.client.interfaces import PORTAL_URL_PLACEHOLDER
+from zope.component import getUtility
 
 
 class AddFavorite(BrowserView):
@@ -36,3 +43,54 @@ class AddFavorite(BrowserView):
             if id_ not in existing_ids:
                 return id_
             idx += 1
+
+
+class RemoteAddFavoriteAction(BrowserView):
+    """This view adds a favorite (Link object) on the remote site called
+    "dashboard" over the bridge proxy. The link object points to the current
+    context.
+    """
+
+    def __call__(self):
+        self._create_favorite(title=self.context.Title(),
+                              url=self._get_url())
+
+        referer = self.request.environ.get('HTTP_REFERER')
+        if referer:
+            self.request.RESPONSE.redirect(referer)
+        else:
+            self.request.RESPONSE.redirect(self.context.absolute_url())
+
+    def _create_favorite(self, title, url):
+        params = {'title': title,
+                'url': url}
+
+        requester = getUtility(IBridgeRequest)
+        try:
+            response = requester('dashboard', '@@add-favorite',
+                                 params=params)
+
+        except MaintenanceError:
+            IStatusMessage(self.request).addStatusMessage(
+                MAINTENANCE_ERROR_MESSAGE, type='error')
+            return False
+
+        if response.status_code == 200:
+            IStatusMessage(self.request).addStatusMessage(
+                _(u'info_favorite_created',
+                  default=u'${title} was added to your favorites.',
+                  mapping={'title': title}),
+                type='info')
+            return True
+
+        else:
+            IStatusMessage(self.request).addStatusMessage(
+                _(u'favorite_creation_failed',
+                  default=u'The favorite could not be created.'),
+                type='error')
+            return False
+
+    def _get_url(self):
+        portal_url = getToolByName(self.context, 'portal_url')() + '/'
+        relative_path = self.context.absolute_url()[len(portal_url):]
+        return ''.join((PORTAL_URL_PLACEHOLDER, relative_path))
--- ftw/bridge/client/tests/test_favorite.py
+++ ftw/bridge/client/tests/test_favorite.py
@@ -1,7 +1,17 @@
+from AccessControl import SecurityManagement
+from AccessControl.users import SimpleUser
 from Products.CMFCore.utils import getToolByName
+from Products.statusmessages.interfaces import IStatusMessage
+from StringIO import StringIO
+from ftw.bridge.client.interfaces import PORTAL_URL_PLACEHOLDER
+from ftw.bridge.client.testing import EXAMPLE_CONTENT_LAYER
 from ftw.bridge.client.testing import INTEGRATION_TESTING
+from ftw.testing import MockTestCase
+from mocker import ARGS, KWARGS, ANY
 from plone.mocktestcase.dummy import Dummy
+from requests.models import Response
 from unittest2 import TestCase
+from zope.component import getMultiAdapter
 
 
 class TestAddWatcherPortletView(TestCase):
@@ -53,3 +63,156 @@ class TestAddWatcherPortletView(TestCase):
         view = self.portal.restrictedTraverse('@@add-favorite')
         self.assertEqual(view.generate_favorite_id(mock_folder),
                          'favorite-3')
+
+
+class TestRemoteAddFavoriteAction(MockTestCase):
+
+    layer = EXAMPLE_CONTENT_LAYER
+
+    def setUp(self):
+        MockTestCase.setUp(self)
+
+        self.requests = self.mocker.replace('requests')
+        # Let the "requests" packaage not do any requests at all while
+        # testing. We do this by expecting any request call zero times.
+        self.expect(self.requests.request(ARGS, KWARGS)).count(0)
+
+        user = SimpleUser('john.doe', 'pw', [], [])
+        SecurityManagement.newSecurityManager(object(), user)
+
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.page = self.portal.get('feed-folder').get('page')
+        self.referer_url = 'http://nohost/plone/feed-folder/page'
+        self.request.environ['HTTP_REFERER'] = self.referer_url
+
+    def tearDown(self):
+        IStatusMessage(self.request).show()
+        if 'HTTP_REFERER' in self.request.environ:
+            del self.request.environ['HTTP_REFERER']
+        SecurityManagement.noSecurityManager()
+
+    def _create_response(self, status_code=200, raw='response data'):
+        response = Response()
+        response.status_code = status_code
+        response.raw = StringIO(raw)
+        return response
+
+    def test_component_is_registered(self):
+        self.replay()
+        getMultiAdapter((self.page, self.request),
+                        name='remote-add-favorite')
+
+    def test_requests_bridge(self):
+        favorite_url = '%sfeed-folder/page' % PORTAL_URL_PLACEHOLDER
+        bridge_url = 'http://bridge/proxy/dashboard/@@add-favorite'
+
+        self.expect(self.requests.request(
+                ANY, bridge_url, headers=ANY,
+                params={'title': 'The page',
+                        'url': favorite_url})).result(
+            self._create_response(raw='OK'))
+
+        self.replay()
+        view = getMultiAdapter((self.page, self.request),
+                               name='remote-add-favorite')
+        view()
+
+    def test_status_message_on_success(self):
+        self.expect(self.requests.request(ANY, ANY, KWARGS)).result(
+            self._create_response(raw='OK'))
+
+        self.replay()
+        view = getMultiAdapter((self.page, self.request),
+                               name='remote-add-favorite')
+        view()
+
+        messages = IStatusMessage(self.request).show()
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message,
+                         u'The page was added to your favorites.')
+        self.assertEqual(messages[0].type,
+                         'info')
+
+    def test_redirects_back(self):
+        self.expect(self.requests.request(ANY, ANY, KWARGS)).result(
+            self._create_response(raw='OK'))
+
+        self.replay()
+        view = getMultiAdapter((self.page, self.request),
+                               name='remote-add-favorite')
+        view()
+
+        self.assertEqual(self.request.response.headers.get('location'),
+                         self.page.absolute_url())
+
+    def test_maintenance_status_message(self):
+        self.expect(self.requests.request(ANY, ANY, KWARGS)).result(
+            self._create_response(status_code=503, raw='Maintenance'))
+
+        self.replay()
+        view = getMultiAdapter((self.page, self.request),
+                               name='remote-add-favorite')
+        view()
+
+        messages = IStatusMessage(self.request).show()
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message,
+                         u'The target service is currently in ' + \
+                             u'maintenace. Try again later.')
+        self.assertEqual(messages[0].type,
+                         'error')
+
+    def test_maintenance_redirect(self):
+        self.expect(self.requests.request(ANY, ANY, KWARGS)).result(
+            self._create_response(status_code=503, raw='Maintenance'))
+
+        self.replay()
+        view = getMultiAdapter((self.page, self.request),
+                               name='remote-add-favorite')
+        view()
+
+        self.assertEqual(self.request.response.headers.get('location'),
+                         self.page.absolute_url())
+
+    def test_error_status_message(self):
+        self.expect(self.requests.request(ANY, ANY, KWARGS)).result(
+            self._create_response(status_code=500, raw='Error'))
+        self.replay()
+
+        view = getMultiAdapter((self.page, self.request),
+                               name='remote-add-favorite')
+        view()
+
+        messages = IStatusMessage(self.request).show()
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message,
+                         u'The favorite could not be created.')
+        self.assertEqual(messages[0].type,
+                         'error')
+
+    def test_error_redirect(self):
+        self.expect(self.requests.request(ANY, ANY, KWARGS)).result(
+            self._create_response(status_code=500, raw='Error'))
+
+        self.replay()
+        view = getMultiAdapter((self.page, self.request),
+                               name='remote-add-favorite')
+        view()
+
+        self.assertEqual(self.request.response.headers.get('location'),
+                         self.page.absolute_url())
+
+    def test_redirects_to_context_when_no_referer_is_found(self):
+        if 'HTTP_REFERER' in self.request.environ:
+            del self.request.environ['HTTP_REFERER']
+        self.expect(self.requests.request(ANY, ANY, KWARGS)).result(
+            self._create_response(status_code=500, raw='Error'))
+
+        self.replay()
+        view = getMultiAdapter((self.page, self.request),
+                               name='remote-add-favorite')
+        view()
+
+        self.assertEqual(self.request.response.headers.get('location'),
+                         self.page.absolute_url())
