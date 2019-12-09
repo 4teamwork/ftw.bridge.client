8489226ea50e10f8a2f9c2db586a6f56bab1dbf4
Tina Gerber <tina.gerber@4teamwork.ch>
2019-12-09 15:59:47 +0100

    Provide precompiled bundle

---
+++ build.js
@@ -0,0 +1,8 @@
+({
+    baseUrl: "./ftw/bridge/client/browser/resources",
+    paths: {
+        jquery: "empty:",
+    },
+    name: "main",
+    out: "./ftw/bridge/client/browser/resources/bridge-client-compiled.js"
+})
